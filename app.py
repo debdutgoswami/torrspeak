752c0b41d75f63d3cba73abbfd365d37f2230836
Debdut Goswami <debdutgoswami@gmail.com>
2021-03-14 15:54:49 +0530

    main code

---
+++ app.py
@@ -0,0 +1,88 @@
+from collections import defaultdict
+from flask import Flask, request
+from flask_socketio import SocketIO, join_room
+from flask_cors import CORS
+from database_connections import db_init, add_message_thread, add_message
+
+from typing import List, Dict, Set, Tuple
+
+app = Flask("web_server")
+
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+db = db_init()
+
+page_sids: Dict[str, Set[str]] = defaultdict(set)
+user_to_page: Dict[str, str] = {}
+thread_messages: Dict[str, List[dict]] = defaultdict(list)
+page_threads: Dict[str, List[str]] = defaultdict(list)
+mouse_pos: Dict[str, Tuple[int, int]] = {}
+sids = set()
+
+# own webserver
+@app.route("/")
+def root_page():
+    return "hello"
+
+
+@socketio.on("connect")
+def connected():
+    print(f"client with sid {request.sid} connected")
+
+
+@socketio.on("connect_to_doc")
+def handle_host_connect(msg):
+    sid = request.sid
+    sids.add(sid)
+    user_id = msg["user"]
+    page_url = msg["url"]
+    page_sids[page_url].add(sid)
+    user_to_page[user_id] = page_url
+    print(f"client {user_id} connected to page {page_url}")
+
+    # Send party ID back to the host
+    socketio.emit("connect_to_doc", f"connected to doc {page_url}!", room=sid)
+
+
+def _add_msg_content(msg):
+    def add_thread():
+        user_id = msg["user"]
+        page_url = user_to_page[user_id]
+        page_threads[page_url].append(msg["message_thread_id"])
+        add_message_thread(
+            db, {"x": msg["x"], "y": msg["y"], "id": msg["message_thread_id"]}
+        )
+
+    thread_id = msg["message_thread_id"]
+    if thread_id not in thread_messages:
+        add_thread()
+
+    new_msg = {"user": msg["user"], "body": msg["body"], "timestamp": msg["timestamp"]}
+    thread_messages[msg["message_thread_id"]].append(new_msg)
+    add_message(db, new_msg, msg["message_thread_id"])
+
+
+@socketio.on("add_msg")
+def add_msg(msg):
+    user_id = msg["user"]
+    print(f"{user_id} sent msg: {msg}")
+    _add_msg_content(msg)
+
+    # Send party ID back to the all users connected to the doc except for sending user
+    for sid in sids:
+        if sid != request.sid:
+            socketio.emit("add_msg", msg, room=sid)
+
+
+@socketio.on("move_cursor")
+def move_cursor(msg):
+    user_id = msg["user"]
+    mouse_pos[user_id] = (msg["x"], msg["y"])
+
+    for sid in sids:
+        if sid != request.sid:
+            socketio.emit("move_cursor", msg, room=sid)
+
+
+if __name__ == "__main__":
+    socketio.run(app, host="localhost", port=8080)
---
+++ database_connections.py
@@ -0,0 +1,54 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import os
+
+
+def db_init():
+    os.environ[
+        "GOOGLE_APPLICATION_CREDENTIALS"
+    ] = "./convbull-firebase-adminsdk-zfqmc-3a1d316ad1.json"
+
+    # Use a service account
+    cred = credentials.Certificate("./convbull-firebase-adminsdk-zfqmc-3a1d316ad1.json")
+    firebase_admin.initialize_app(cred)
+
+    db = firestore.Client()
+    return db
+
+
+def add_message_thread(db, msg_thread):
+    doc_ref = db.collection(u"message-threads").document(msg_thread["id"])
+    doc_ref.set(
+        {
+            u"x": msg_thread["x"],
+            u"y": msg_thread["y"],
+            u"message_thread_id": msg_thread["id"],
+            u"messages": [],
+        }
+    )
+
+
+def add_message(db, msg, msg_thread_id):
+    doc_ref = db.collection(u"message-threads").document(msg_thread_id)
+    doc_ref.update(
+        {
+            u"messages": firestore.ArrayUnion(
+                [
+                    {
+                        u"user": msg["user"],
+                        u"body": msg["body"],
+                        u"timestamp": msg["timestamp"],
+                    }
+                ]
+            )
+        }
+    )
+
+
+def get_message_threads(db):
+    # Then query for documents
+    message_threads_ref = db.collection(u"message-threads")
+
+    for doc in message_threads_ref.stream():
+        print(u"{} => {}".format(doc.id, doc.to_dict()))
